c94d1d36b1a66856ffa0b9076bd60c08ef66b3fb
Alirio-Mieres <mie21001@byui.edu>
2021-10-19 18:12:47 -0400

    week6-1

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -13,3 +13,12 @@ let date = event.toLocaleDateString('en-UK', options);
 
 document.querySelector("#lastupdated").textContent = date;
 
+
+const saturday = ["Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."];
+
+
+document.querySelector("#message").textContent = saturday;
+
+if(event.getDay() == 5){
+  document.querySelector(".message").style.display = "block";
+}
