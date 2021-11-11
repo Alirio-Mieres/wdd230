f668b092623fb998591c4e58cf9fe887ec5b5db3
Alirio-Mieres <mie21001@byui.edu>
2021-11-11 19:46:44 -0400

    week9

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,16 +6,39 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+
     for (let i = 0; i < prophets.length; i++ ) {
+
       let card = document.createElement('section');
+
       let h2 = document.createElement('h2');
 
-      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      let birthdate = document.createElement('p');
+      
+      let birthplace = document.createElement('p');
+
+      let imagesProphets = document.createElement('img');
+
+      h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
+
+      birthdate.textContent = `Date of Birth: ${prophets[i].birthdate}`;
+     
+      birthplace.textContent = `Place of Birth: ${prophets[i].birthplace}`;
+      
+      imagesProphets.setAttribute( 'src', prophets[i].imageurl);
+
+      infoProphets = `${prophets[i].name}  ${prophets[i].lastname} - ${prophets[i].order}`;
+
+      imagesProphets.setAttribute( 'alt', infoProphets);
 
       card.appendChild(h2);
+      card.appendChild(birthdate);
+      card.appendChild(birthplace);
+      card.appendChild(imagesProphets);
 
       document.querySelector('div.cards').appendChild(card);
     }});
 
-const prophets = jsonObject['prophets'];
+
 
